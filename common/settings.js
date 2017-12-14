72bf2c4fbeccc09df0d212855eb743cf67e422b9
Xavier Urzúa <xavier.urzua.acuna@gmail.com>
2017-12-14 14:27:09 -0300

    moved isParameterObject() function to the bottom

--- common/settings.js
+++ common/settings.js
@@ -8,46 +8,6 @@ var settings = null;
 //--------------------------------------------------------------------------------------
 // SETTINGS FUNCTIONS
 //--------------------------------------------------------------------------------------
-const getSettings = () => {
-    if (isSettingsFileExist()) {
-        if (!isSettingsFileEmpty()) {
-            return settings;
-        }
-    }
-
-    throw ("There is no configuration file in the current directory. Check if the settings.json file exist!");
-};
-
-const isSettingsFileExist = () => {
-    try {
-        fs.statSync("settings.json").isFile();
-        settings = require("../settings.json");
-        return true;
-    } catch (err) {
-        return false;
-    }
-};
-
-const isSettingsFileEmpty = () => {
-
-    if (isParameters(settings)) {
-        Object.values(settings).forEach(key => {
-            let parameters = key.parameters;
-            Object.values(parameters).forEach(key => {
-                if (isParameterObject(key)) {
-                    if (isParameterObjectEmpty(key)) {
-                        throw ("Check you settings.json file, there is a missing, null or undefined parameter in your configuration file");
-                    }
-                } else {
-                    if (isParameterEmpty(key)) {
-                        throw ("Check you settings.json file, there is a missing, null or undefined parameter in your configuration file");
-                    }
-                }
-            });
-        });
-    }
-    return false;
-};
 
 const isParameterObject = (obj) => {
     if (typeof obj === "object") {
@@ -85,6 +45,48 @@ const isParameterEmpty = (key) => {
         return true;
     }
     return false;
+};
+
+
+const isSettingsFileExist = () => {
+    try {
+        fs.statSync("settings.json").isFile();
+        settings = require("../settings.json");
+        return true;
+    } catch (err) {
+        return false;
+    }
+};
+
+const isSettingsFileEmpty = () => {
+
+    if (isParameters(settings)) {
+        Object.values(settings).forEach(key => {
+            let parameters = key.parameters;
+            Object.values(parameters).forEach(key => {
+                if (isParameterObject(key)) {
+                    if (isParameterObjectEmpty(key)) {
+                        throw ("Check you settings.json file, there is a missing, null or undefined parameter in your configuration file");
+                    }
+                } else {
+                    if (isParameterEmpty(key)) {
+                        throw ("Check you settings.json file, there is a missing, null or undefined parameter in your configuration file");
+                    }
+                }
+            });
+        });
+    }
+    return false;
+};
+
+const getSettings = () => {
+    if (isSettingsFileExist()) {
+        if (!isSettingsFileEmpty()) {
+            return settings;
+        }
+    }
+
+    throw ("There is no configuration file in the current directory. Check if the settings.json file exist!");
 };
 
 //--------------------------------------------------------------------------------------
